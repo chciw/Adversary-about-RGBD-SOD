a7ed6973d1ae9106e7879f9e6dc9ae144f26f632
chciw <43063018+chciw@users.noreply.github.com>
2021-08-14 16:49:13 +0800

    Update SWS.py

--- SWS.py
+++ SWS.py
@@ -9,9 +9,10 @@ import numpy as np
 
 def SWS(img):
     image = img_as_float(cv2.imread(img)) # w=378 h=596
-    numSegments=40  # 400->500x500 np.unique(seg)总是比这里指定的少
+    numSegments=40   
     segments = slic(image, n_segments=numSegments, sigma=5,start_label=1) # (596,378) (h,w)
-
+    print("np.unique(segments):", np.unique(segments))
+    
     # plt_img=io.imread(img)
     # plt.subplot(131)
     # plt.title('image')
@@ -24,23 +25,17 @@ def SWS(img):
     # plt.imshow(mark_boundaries(plt_img, segments))
     # plt.show()
 
-    # print("segments:\n", segments)
-    print("np.unique(segments):", np.unique(segments))
-
-    # output=np.zeros(image.shape,dtype='float64')
-    # cv2.imshow('origin', image)
     for (i, segVal) in enumerate(np.unique(segments)):
-        # 为segment构造超像素mask
-        # print("[x] inspecting segment {}, for {}".format(i, segVal))
+        # 为每个超像素构造mask
         mask = np.zeros(image.shape[:2], dtype="uint8")
         mask[segments == segVal] = 255
         # cv2.imshow("Mask", mask)
-
         sPixel=np.multiply(image, cv2.cvtColor(mask, cv2.COLOR_GRAY2RGB) > 0)
         # cv2.imshow("Applied", sPixel)
+        
         # 在mask范围内打乱像素
-        h_sp=np.argwhere(mask==255)[:,0] # 所有行的第0个数据
-        w_sp=np.argwhere(mask==255)[:,1] # 所有行的第1个数据
+        h_sp=np.argwhere(mask==255)[:,0] # 所有坐标的第0个数据
+        w_sp=np.argwhere(mask==255)[:,1] # 所有坐标的第1个数据
         h_min,h_max=np.min(h_sp),np.max(h_sp)
         w_min,w_max=np.min(w_sp),np.max(w_sp)
         n_swap=(h_max-h_min)*(w_max-w_min)//20 # 像素交换次数
@@ -59,18 +54,14 @@ def SWS(img):
                 image[h1,w1,:]=image[h2,w2,:]
                 image[h2,w2,:]=tmp
             n_swap=n_swap-1
-        # cv2.imshow('sp',np.multiply(image, cv2.cvtColor(mask, cv2.COLOR_GRAY2RGB) > 0))
-        # cv2.waitKey(0)
-        # output+=sPixel
-        # cv2.waitKey(0)
+
     # cv2.imshow('SWS',image)
     # cv2.waitKey(0)
     cv2.imwrite(rootdir + 'SWSDeep/' + file, image*255)
     # return image
 
 rootdir = './dataset/NLPR/test/rosa/'
-# input_w=320
-# input_h=320
+# input_w,input_h=320,320
 
 # list=[]
 # f=open('extracted.txt')
@@ -88,8 +79,10 @@ for root, dirs, files in os.walk(rootdir+'deep/'):
                 #     continue
                 if os.path.splitext(file)[1]=='.txt':
                         continue
+                SWS(rootdir+'deep/'+file)
+                
                 # img = cv2.imread(rootdir+'after/'+file)
                 # img = cv2.resize(img, (input_w, input_h), interpolation=cv2.INTER_CUBIC)
                 # cv2.imwrite(rootdir+'GT/'+file,img)
-                SWS(rootdir+'deep/'+file)
+                
 
